616302892f399edecb9a0cc23f80856f7fa28286
Zhou Shunchao <zhoushunchao@Zhous-MacBook-Pro.local>
2025-11-18 10:53:02 +0800

    Add report tool: Generate investment-banking style HTML reports with relationship graphs and probability analysis

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -3,6 +3,7 @@
 from .default_tools import *
 from .workflow_tools import *
 from .mcp_tools import *
+from .report import *
 from .protocol import tcp
 
 __all__ = [
---
+++ src/tools/report/__init__.py
@@ -0,0 +1,9 @@
+"""Report tool for generating investment-banking style HTML reports."""
+
+from .report import ReportTool, ReportToolArgs
+
+__all__ = [
+    "ReportTool",
+    "ReportToolArgs",
+]
+
